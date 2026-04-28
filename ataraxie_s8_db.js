e97e0671f38790072600e7b069df6fd015254474
MohamedOuHouiss <69607762+mo74meed@users.noreply.github.com>
2026-04-28 12:12:09 +0100

    fx

--- ataraxie_s8_db.js
+++ ataraxie_s8_db.js
@@ -11320,7 +11320,7 @@ const ATARAXIE_S8_DB = {
                                     "id": "CHO-MAL-12-Q07",
                                     "clinical_context": null,
                                     "question_text": "Les scolioses :\n-Données de l’examen clinique\n-Critères cliniques et radiologiques d'évaluation et de suivi d'une scoliose.\n-Circonstances de découverte et signes cliniques\-Étiologies",
-                                    "type": "QCM",
+                                    "type": "Redaction",
                                     "choices": null,
                                     "requires_review": false
                                 },
